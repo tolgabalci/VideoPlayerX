3c27b0c84f1cdbe9c703b55aae5aaafb1c6983a1
tolgabalci <tolga.balci@gmail.com>
2017-10-26 12:06:06 -0400

    Initial Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from "angularfire2/auth";
+import { Observable } from 'rxjs/Observable';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-root',
@@ -6,13 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user$: Observable<firebase.User>;
   title = 'app';
 
-  Login() {
+  constructor(private auth: AngularFireAuth) {
+    this.user$ = auth.authState;
+  }
 
+  Login() {
+    this.auth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
 
   Logout() {
-    
+    this.auth.auth.signOut();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppComponent } from './app.component';
+
+var firebaseConfig = {
+  "apiKey": "AIzaSyBcWifa9JKUuSrEMTJQm35QUGKZrZf7KCw",
+  "databaseURL": "https://tolgalist.firebaseio.com",
+  "storageBucket": "tolgalist.appspot.com",
+  "authDomain": "tolgalist.firebaseapp.com",
+  "messagingSenderId": "610637531922",
+  "projectId": "tolgalist"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
